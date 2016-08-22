acb4c847f86a8f80d172c938f7a72f94c3578910
chuby1tubby <knakamura13@apu.edu>
2016-08-22 15:11:20 -0700

    hey

---
+++ Pods/Target Support Files/Pods-Rainy Shiny Weather App/Pods-Rainy Shiny Weather App-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Rainy_Shiny_Weather_AppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Rainy_Shiny_Weather_AppVersionString[];
+
