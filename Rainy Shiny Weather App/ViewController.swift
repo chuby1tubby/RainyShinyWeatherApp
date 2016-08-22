204cecd7501d4f9b32522fe29cefce810eae9ab0
chuby1tubby <knakamura13@apu.edu>
2016-08-22 14:29:58 -0700

    Initial Commit

---
+++ Rainy Shiny Weather App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rainy Shiny Weather App
+//
+//  Created by Kyle Nakamura on 8/22/16.
+//  Copyright © 2016 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
